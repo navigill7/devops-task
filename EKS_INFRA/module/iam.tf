6adc16168969cc0012e85b78daf320e5dc1515e7
Navinder Gill <nnavigill784@gmail.com>
2025-09-13 16:23:42 +0530

    adding the iam configurations for eks cluster

---
+++ EKS_INFRA/module/iam.tf
@@ -0,0 +1,100 @@
+locals {
+  cluster-name = var.cluster-name
+}
+
+resource "random_integer" "random_suffix" {
+  min = 1000
+  max = 9999
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  count = var.is_eks_role_enabled ? 1 : 0
+  name  = "${local.cluster-name}-role-${random_integer.random_suffix.result}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  count      = var.is_eks_role_enabled ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role[count.index].name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
+  count      = var.is_eks_role_enabled ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_cluster_role[count.index].name
+  
+}
+
+// IAM role for worker nodes
+resource "aws_iam_role" "eks_nodegroup_role" {
+  count = var.is_eks_nodegroup_role_enabled ? 1 : 0
+  name  = "${local.cluster-name}-nodegroup-role-${random_integer.random_suffix.result}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonWorkerNodePolicy" {
+  count      = var.is_eks_nodegroup_role_enabled ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodegroup_role[count.index].name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKS_CNI_Policy" {
+  count      = var.is_eks_nodegroup_role_enabled ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodegroup_role[count.index].name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEC2ContainerRegistryReadOnly" {
+  count      = var.is_eks_nodegroup_role_enabled ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodegroup_role[count.index].name
+}
+
+// IAM Role for OIDC
+resource "aws_iam_role" "eks_oidc_role" {
+  assume_role_policy = data.aws_iam_policy_document.eks-oidc-document.json
+  name               = "eks-oidc"
+}
+
+resource "aws_iam_policy" "eks_oidc_policy" {
+  name = "oidc-policy"
+
+  policy = jsonencode({
+    Statement = [{
+      Action = [
+        "s3:ListAllMyBuckets",
+        "s3:GetBucketLocation",
+        "*"
+      ]
+      Effect   = "Allow"
+      Resource = "*"
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_oidc_policy_attach" {
+  role       = aws_iam_role.eks_oidc_role.name
+  policy_arn = aws_iam_policy.eks_oidc_policy.arn
+}
